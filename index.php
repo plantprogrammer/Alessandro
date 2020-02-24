cb1da9236a89fa1d64c0a5087ff7c2786dfa26f1
Ian <ian.sajkowicz@uconn.edu>
2020-02-24 14:29:53 -0500

    Correcting the Errors that Appeared

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 
-if((have_posts()):
-	while(have_posts()): the_post();
+if(have_posts()):
+	while(have_posts()): the_post();?>
 	
 	<h2><?php the_title()?></h2>
 	<?php the_content()?>
 	
-	<?php endwhile;?>
+	<?php endwhile;
 	
 else:
 	echo "<p>There are no posts to display!</p>";
