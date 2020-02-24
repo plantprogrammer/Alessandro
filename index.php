2febbe94a3ecb952eea1358b0ec5711777360c1f
Ian <ian.sajkowicz@uconn.edu>
2020-02-24 14:26:52 -0500

    Add files via upload
    Now It Displays Something More Interesting

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-<h1>Hello!</h1>
+<?php
+
+if((have_posts()):
+	while(have_posts()): the_post();
+	
+	<h2><?php the_title()?></h2>
+	<?php the_content()?>
+	
+	<?php endwhile;?>
+	
+else:
+	echo "<p>There are no posts to display!</p>";
+
+endif;
+
+?>
