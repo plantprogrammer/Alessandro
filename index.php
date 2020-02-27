5dab07af0b29b04713b34eeb3d941e9aae7b8a72
Ian <ian.sajkowicz@uconn.edu>
2020-02-26 19:38:33 -0500

    Update index.php
    Included a comment

--- index.php
+++ index.php
@@ -1,13 +1,23 @@
 <?php
-
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ */
 get_header();
 
 if(have_posts()):
 	while(have_posts()): the_post();?>
-	
+
+	<article class = "page">
 <h2><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
 	<?php the_content();?>
-	
+	</article>
+
 	<?php endwhile;
 	
 else:
